cac5485d981a8bb310c4c1efd7ce11681be4e397
Romil <47531544+Shisoo64@users.noreply.github.com>
2020-06-04 13:49:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+var json = $.getJSON("restaurant.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+}); 
+*/
+
 var json = 
 [
    {
@@ -34,28 +40,18 @@ var json =
    }
 ];
 
-
-
-
-
 $(document).ready(function() { 
 
-// Loop dans le Json
-  for(var i = 0; i < json.length; i++) {
-    var obj = json[i];
-	// Ajout du restaurant et de son adresse
-    $('<p class="h5">' + obj.restaurantName + '</p>' +
-    	'<p>' + obj.address + '</p>').appendTo('#list');
 
-    // Ajout des ratings
-    for (e = 0; e < obj.ratings.length; e++) {
-		$('<p>' + obj.ratings[e].comment + '</p> <p class="h4">' + obj.ratings[e].stars + '/5</p>').appendTo('#list');
-    }
+  initRatings(0,5);
 
-    // Fin du restaurant
-    $('<hr>').appendTo('#list');
+  $('#minStars').change(function() {
+    initRatings($(this).val(),$('#maxStars').val());
+  });
 
-  }
+  $('#maxStars').change(function() {
+    initRatings($('#minStars').val(),$(this).val());
+  });
 
 });
 
@@ -63,7 +59,39 @@ $(document).ready(function() {
 
 
 
-  function initMap() {
+
+function initRatings(minStars, maxStars) {
+
+  $('#list').empty();
+
+  // Loop dans le Json
+  for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+    var moyStars = 0;
+
+    // Loop Moy
+    for (e = 0; e < obj.ratings.length; e++) {
+      moyStars = moyStars + obj.ratings[e].stars;
+    }
+    moyStars = moyStars / obj.ratings.length;
+
+      if(moyStars >= minStars && moyStars <= maxStars){
+        // Ajout du restaurant et de son adresse
+        $('<p class="h5">' + obj.restaurantName + '</p>' +
+          '<p>' + obj.address + '</p>').appendTo('#list');
+        // Ajout des ratings
+        for (e = 0; e < obj.ratings.length; e++) {
+        $('<p>' + obj.ratings[e].comment + '</p> <p class="h4">' + obj.ratings[e].stars + '/5</p>').appendTo('#list');
+        }
+      }
+    // Fin du restaurant
+    $('<hr>').appendTo('#list');
+  }
+}
+
+
+
+function initMap() {
 
   gMap = new google.maps.Map(document.getElementById('map'));
 
@@ -72,22 +100,28 @@ $(document).ready(function() {
     var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     gMap.setCenter(initialLocation);
     gMap.setZoom(13);
+    newMarker(initialLocation, "Vous");
   }, function(positionError) {
     // Sinon centrage sur la tour eiffel
     gMap.setCenter(new google.maps.LatLng(48.858382, 2.294480));
     gMap.setZoom(12);
+    newMarker(new google.maps.LatLng(48.858382, 2.294480), "Vous");
   });
 
-
-// Loop dans le Json
+  // Loop dans le Json
   for(var i = 0; i < json.length; i++) {
     var obj = json[i];
-	// Ajout d'un marker
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(obj.lat,obj.long),
-      map: gMap,
-      title: obj.restaurantName
-    });
+    newMarker(new google.maps.LatLng(obj.lat,obj.long), obj.restaurantName);
   }
 
+}
+
+
+
+function newMarker(position, title) {
+  var marker = new google.maps.Marker({
+      position: position,
+      map: gMap,
+      title: title
+    });
 }

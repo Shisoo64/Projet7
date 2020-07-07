099ebbc7b1f83cbdb35899d173d145cef8af8543
Romil <Shisoo64@gmail.com>
2020-07-07 15:09:06 +0200

    Update css et debut Places Api

--- js/main.js
+++ js/main.js
@@ -12,10 +12,26 @@ $('#maxStars').change(function() {
 resto.moyChange($('#minStars').val(),$(this).val());
 });
 
-$(".ratingAdd").click(function() {
+
+
+$(document).on("click", ".ratingAdd", function() {
 	$('#ratingsModal').modal('show');
-    $("#ratingsModalOk").click(function() {
-      resto.createRating($("#ratingsModalStars").val(), $("#ratingsModalStars").val(), $("#ratingsModalText").val(), this.getAttribute('data-value'));
-      $('#exampleModal').modal('hide');
-    });
+	$("#ratingsModalOk").attr('data-value', this.getAttribute('data-value'));
 });
+
+$("#ratingsModalOk").click(function() {
+	console.log(this.getAttribute('data-value'));
+	resto.createRating($("#ratingsModalStars").val(), $("#ratingsModalStars").val(), $("#ratingsModalText").val(), this.getAttribute('data-value'));
+	$('#ratingsModal').modal('hide');
+});
+
+
+
+
+
+
+
+      console.log("Truc");
+    $.getJSON("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.950030,14.537220&radius=500&type=restaurant&key=AIzaSyDoAgu5pb6ODIxcuiG-8Ls9AgMhaGBebRU&callback=?", function(data){
+      console.log("Data: " + data);
+    })
--- js/resto.js
+++ js/resto.js
@@ -13,6 +13,8 @@ class Resto {
   initResto() {
     $('#list').empty();
     $.getJSON("restaurant.json", function(json) {
+    //$.getJSON("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.950030,14.537220&radius=500&type=restaurant&key=AIzaSyDoAgu5pb6ODIxcuiG-8Ls9AgMhaGBebRU", function(json) {
+      //console.log(json);
       // Each restaurant
       $.each(json, function(i, restaurant){
         // Calcul moy restaurant
@@ -34,17 +36,16 @@ class Resto {
   }
 
   createResto(lat, long, moy, address, restaurantName) {
-    $('<a class="btn btn-primary btn-lg btn-block restaurant restaurantBtn" data-value="'+ moy +'" data-toggle="collapse" data-target="#collapse' + restaurantName + '" role="button" aria-expanded="false" aria-controls="collapseExample">' + restaurantName + '</a><p class="lead mb-4">' + address + '</p>').appendTo('#list');
+    $('<a class="btn btn-primary btn-lg btn-block mt-3 restaurant restaurantBtn" data-value="'+ moy +'" data-toggle="collapse" data-target="#collapse' + restaurantName + '" role="button" aria-expanded="false" aria-controls="collapseExample">' + restaurantName + '</a><p class="lead mb-4">' + address + '</p>').appendTo('#list');
     map.newMarker(new google.maps.LatLng(lat,long), restaurantName);
     // Div collapse
     $('<div class="collapse" id="collapse' + restaurantName + '"></div>').appendTo('#list');
     $('<img class="img-fluid mb-4" src="https://maps.googleapis.com/maps/api/streetview?size=900x300&location=' + lat + ',' + long + '&fov=80&heading=70&pitch=0&key=AIzaSyDoAgu5pb6ODIxcuiG-8Ls9AgMhaGBebRU">').appendTo('#collapse' + restaurantName);
-    $('<button type="button" data-value="'+ restaurantName +'" class="btn btn-secondary btn-sm mb-3 ratingAdd">Ajouter</button>').appendTo('#collapse' + restaurantName);
-
+    $('<button type="button" data-value="'+ restaurantName +'" class="btn btn-outline-secondary btn-sm btn-block mb-2 ratingAdd">Ajouter un avis</button>').appendTo('#collapse' + restaurantName);
   }
 
   createRating(moy, stars, comment, restaurantName){
-    $('<div class="restaurant mb-3" data-value="'+ moy+'"><div class="card card-body"><h4>' + stars + '/5 <small class="text-muted">' + comment + '</small></h4></div></div>').appendTo('#collapse' + restaurantName);
+    $('<div class="restaurant mb-2" data-value="'+ moy+'"><div class="card card-body"><h4>' + stars + '/5 <small class="text-muted">' + comment + '</small></h4></div></div>').appendTo('#collapse' + restaurantName);
   }
 
 }
